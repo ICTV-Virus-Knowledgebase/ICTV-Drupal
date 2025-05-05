8191d2d388d56586477a82636689ad57c2dec937
UAB\ddempsey <97637401+don-dempsey@users.noreply.github.com>
2025-05-05 14:46:20 -0500

    Commenting out unused code.

--- ui/src/components/TaxonomyBrowser/TaxonomyBrowser.ts
+++ ui/src/components/TaxonomyBrowser/TaxonomyBrowser.ts
@@ -1118,10 +1118,10 @@ export class TaxonomyBrowser {
             await this.getReleaseHistory();
             break;
 
-         case TaxonomyDisplayType.display_unassigned_child_taxa:
+         /*case TaxonomyDisplayType.display_unassigned_child_taxa:
             await this.getRelease(this.initialData.releaseNumber);
             await this.getUnassignedChildTaxaByName();
-            break;
+            break;*/
 
          default:
             await this.getRelease(this.initialData.releaseNumber);
